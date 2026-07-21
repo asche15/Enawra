3c66f6de3beb30ced8bbe9567bba1a2d43ced431
Aschalew <aschalew.tesfaye15@gmail.com>
2026-07-21 18:58:45 +0300

    adding docker file

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,9 +5,13 @@ import User from './models/User.js'
 import { ConnectDb } from "./lib/connectDb.js";
 import { clerkMiddleware } from '@clerk/express'
 
+import fs from "fs"
+import path from "path"
+
 dotenv.config()
 const app = express()
 const PORT =process.env.PORT||3000
+const publicDir = path.join(process.cwd(), "public")
 const frontendUrl = process.env.FRONTEND_URL
 
 app.use(express.json())
@@ -17,6 +21,15 @@ app.use(cors({origin:frontendUrl, credentials:true}))
 app.get("/health", (req, res) => {
   res.status(200).json({ok: true});
 })
+
+
+if(fs.existsSync(publicDir)){
+   app.use(express.static(publicDir))
+CHAT_APP03
+   app.get("/{*any}", (req, res, next) =>{
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 
 app.listen(PORT, ()=>{
 
